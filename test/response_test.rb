53cf957668dbc8ae5efba3bd0ce9f3fa48ae9b0f
Kerry <kerry.sheldon@gmail.com>
2016-04-12 14:47:06 -0600

    screw up

--- test/response_test.rb
+++ test/response_test.rb
@@ -12,6 +12,8 @@ class ResponseTest < MiniTest::Test
   end
 
 
+["GET", "/", "HTTP/1.1", "127.0.0.1:9292", "keep-alive", "no-cache", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "caad7182-899e-751e-acc0-dab87a96eca0", "*/*", "gzip, deflate, sdch", "en-US,en;q=0.8"]
+
 
 
 
