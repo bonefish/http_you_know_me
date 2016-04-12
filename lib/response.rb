4521f97d9718dd431ed5c12c932df5a95a4e42e3
Kerry <kerry.sheldon@gmail.com>
2016-04-12 15:53:10 -0600

    refactored response to handle hash and added path functionality

--- lib/response.rb
+++ lib/response.rb
@@ -3,21 +3,20 @@ require 'faraday'
 
 class Response
 
-  #remove, this was the experiment section of readme
-  def experiment_response(request_lines)
-    "<pre>" + request_lines.join("\n") + "</pre>"
-  end
-
   def hello_response(count)
     "<pre>Hello, World!(#{count})</pre>"
   end
 
-  def diagnostics_response(request_lines)
-    "<pre>Verb: #{parse_verb(request_lines)}\nPath: #{parse_path(request_lines)}\nProtocol: #{parse_protocol(request_lines)}\nHost: #{parse_host(request_lines)}\nPort: #{parse_port(request_lines)}\nOrigin: #{parse_origin(request_lines)}\nAccept: #{parse_accept(request_lines)}\n</pre>"
+  def diagnostic_response(request_hash)
+    "<pre>#{format_diagnostic_response(request_hash)}</pre>"
   end
 
-  def send_experiment_response(connection, request_lines)
-    send_response(connection, experiment_response(request_lines))
+  def format_diagnostic_response(request_hash)
+    diagnostic_string = ""
+    request_hash.each do |key, value|
+      diagnostic_string = diagnostic_string + key + ": " + value + "\n"
+    end
+    diagnostic_string
   end
 
   def send_hello_response(connection, count)
@@ -32,22 +31,23 @@ class Response
     response = id_response_by_path(path, hello_count, request_count)
   end
 
-  def response_by_path(request_lines, path, hello_count, request_count)
+  def response_by_path(path, request_hash, hello_count, request_count)
     if path == "/"
-      diagnostics_response(request_lines)
+      diagnostic_response(request_hash)
     elsif path == "/hello"
       hello_response(hello_count)
-    elsif path =="/datetime"
-      "#{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}"
-    elsif path =="/shutdown"
-      "Total requests: #{request_count}"
+    elsif path == "/datetime"
+      "<pre>#{Time.now.strftime('%l:%M%p on %A, %B %e, %Y ')}</pre>"
+    elsif path == "/shutdown"
+      "<pre>Total requests: #{request_count}</pre>"
     else
-      #???
+      "<pre>Invalid Path</pre>"
     end
   end
 
-
-
+  def find_path(request_hash)
+    request_hash["Path"]
+  end
 
   def send_response(connection, response)
     connection.puts headers(output(response))
@@ -65,37 +65,5 @@ class Response
       "content-type: text/html; charset=iso-8859-1",
       "content-length: #{output.length}\r\n\r\n"].join("\r\n")
   end
-
-
-#change into hash; except first line, key is thing before first :
-  def parse_verb(request_lines)
-    request_lines[0].split(" ")[0]
-  end
-
-  def parse_path(request_lines)
-    request_lines[0].split(" ")[1]
-  end
-
-  def parse_protocol(request_lines)
-    request_lines[0].split(" ")[2]
-  end
-
-  def parse_host(request_lines)
-    request_lines[1].split(":")[1].lstrip!
-  end
-
-  def parse_port(request_lines)
-    request_lines[1].split(":")[2]
-  end
-
-  def parse_origin(request_lines)
-    request_lines[1].split(":")[1].lstrip!
-  end
-
-  def parse_accept(request_lines)
-    request_lines[4].split(":")[1].lstrip!
-  end
-
-
 
 end
--- test/response_test.rb
+++ test/response_test.rb
@@ -1,24 +1,54 @@
-require './lib/request.rb'
+require './lib/response'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class ResponseTest < MiniTest::Test
 
-  def set_up
+  attr_reader :request_lines, :request_hash, :response
+
+  def setup
     @request_lines = ["GET / HTTP/1.1", "Host: 127.0.0.1:9292", "Connection: keep-alive", "Cache-Control: no-cache", "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "Postman-Token: caad7182-899e-751e-acc0-dab87a96eca0", "Accept: */*", "Accept-Encoding: gzip, deflate, sdch", "Accept-Language: en-US,en;q=0.8"]
+
+    @request_hash = {"Verb" => "GET", "Path" => "/", "Protocol" => "HTTP/1.1", "Host" => "127.0.0.1:9292"}
+      # , "Connection"=> "keep-alive", "Cache-Control:" => "no-cache", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "Postman-Token" => "caad7182-899e-751e-acc0-dab87a96eca0"}
+
+    @response = Response.new
+  end
+
+  def test_diagnostic_response_format_is_correct
+    expected = "Verb: GET\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1:9292\n"
+    assert_equal expected, response.format_diagnostic_response(request_hash)
+  end
+
+  def test_diagnostic_response_is_correct
+    expected = "<pre>Verb: GET\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1:9292\n</pre>"
+    assert_equal expected, response.diagnostic_response(request_hash)
   end
 
   def test_hello_world_response_is_correct
-
+    expected = "<pre>Hello, World!(3)</pre>"
+    assert_equal expected, response.hello_response(3)
   end
 
+  def test_response_to_root_is_correct
+    expected = "<pre>Verb: GET\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1:9292\n</pre>"
+    assert_equal expected, response.response_by_path("/", request_hash,  0, 0)
+  end
 
-["GET", "/", "HTTP/1.1", "127.0.0.1:9292", "keep-alive", "no-cache", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36", "caad7182-899e-751e-acc0-dab87a96eca0", "*/*", "gzip, deflate, sdch", "en-US,en;q=0.8"]
+  def test_response_to_hello_is_correct
+    expected = "<pre>Hello, World!(3)</pre>"
+    assert_equal expected, response.response_by_path("/hello", request_hash, 3, 0)
+  end
 
+  def test_response_to_date_is_correct
+    skip #keep because will fail due to current time
+    expected = "<pre>3:43PM on Tuesday, April 12, 2016</pre>"
+    assert_equal expected, response.response_by_path("/datetime", request_hash, 3, 0)
+  end
 
-
-
-
-
+  def test_response_to_shutdown_is_correct
+    expected = "<pre>Total requests: 12</pre>"
+    assert_equal expected, response.response_by_path("/shutdown", request_hash, 3, 12)
+  end
 
 end
